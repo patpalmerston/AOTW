a3c2a0122ce73bb0de733e806ef9928fac533a22
patpalmerston <patpalmerston@gmail.com>
2019-05-16 20:53:35 -0700

    server live

---
+++ api/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const configureMiddleware = require('./middleware');
+
+const server = express();
+
+configureMiddleware(server)
+
+server.get('/', (req, res, next) => {
+  res.send(`<h2>We are Live!<h2>`)
+})
+
+module.exports = server;
